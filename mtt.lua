07248031452a083a96a27502e2a692ebd787e0a6
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-03-19 13:34:18 +0100

    add mineclonia

--- mtt.lua
+++ mtt.lua
@@ -2,6 +2,14 @@
 -- emerge a part of the world (basic smoketest)
 mtt.emerge_area({ x=0, y=0, z=0 }, { x=10, y=10, z=10 })
 
+local nodelist
 if minetest.get_modpath("default") then
-    mtt.validate_nodenames(minetest.get_worldpath() .. "/nodelist/minetest_game.txt")
+    nodelist = "minetest_game"
+elseif minetest.get_modpath("mcl_init") then
+    nodelist = "mineclonia"
+end
+
+if nodelist then
+    -- nodelist available
+    mtt.validate_nodenames(minetest.get_worldpath() .. "/nodelist/" .. nodelist .. ".txt")
 end
