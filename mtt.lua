3827c26fc250a744bf72ad5c43bb876daefa1e33
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-03-19 13:37:47 +0100

    nodelist refactoring

--- mtt.lua
+++ mtt.lua
@@ -2,14 +2,6 @@
 -- emerge a part of the world (basic smoketest)
 mtt.emerge_area({ x=0, y=0, z=0 }, { x=10, y=10, z=10 })
 
-local nodelist
-if minetest.get_modpath("default") then
-    nodelist = "minetest_game"
-elseif minetest.get_modpath("mcl_init") then
-    nodelist = "mineclonia"
-end
-
-if nodelist then
-    -- nodelist available
-    mtt.validate_nodenames(minetest.get_worldpath() .. "/nodelist/" .. nodelist .. ".txt")
-end
+-- check nodelist
+local info = minetest.get_game_info()
+mtt.validate_nodenames(minetest.get_worldpath() .. "/nodelist/" .. info.id .. ".txt")
