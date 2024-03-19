21d1deb0cf0ffb98f99dedb4ff9f56c584d9d3d5
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-03-19 13:14:09 +0100

    rename things

--- mtt.lua
+++ mtt.lua
@@ -3,5 +3,5 @@
 mtt.emerge_area({ x=0, y=0, z=0 }, { x=10, y=10, z=10 })
 
 if minetest.get_modpath("default") then
-    mtt.validate_nodenames(minetest.get_worldpath() .. "/nodelist/default.txt")
+    mtt.validate_nodenames(minetest.get_worldpath() .. "/nodelist/minetest_game.txt")
 end
