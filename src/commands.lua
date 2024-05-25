c46e90d61283be65d4ded37dfa34f58e5f2287be
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2024-05-25 16:01:00 -0400

    add basic textures test

--- src/commands.lua
+++ src/commands.lua
@@ -1,8 +1,15 @@
 local materials_list = minetest.get_dir_list(xcompat.modpath.."/src/materials", false)
 local materials = {}
 for _, material in ipairs(materials_list) do
-    local material_name = material:sub(1, -5)
-    materials[material_name] = dofile(xcompat.modpath.."/src/materials/"..material)
+    local gameid = material:sub(1, -5)
+    materials[gameid] = dofile(xcompat.modpath.."/src/materials/"..material)
+end
+
+local textures_list = minetest.get_dir_list(xcompat.modpath.."/src/textures", false)
+local textures = {}
+for _, texture in ipairs(textures_list) do
+    local gameid = texture:sub(1, -5)
+    textures[gameid] = dofile(xcompat.modpath.."/src/textures/"..texture)
 end
 
 minetest.register_chatcommand("xcompat_test_materials", {
@@ -20,5 +27,25 @@ minetest.register_chatcommand("xcompat_test_materials", {
         end
 
         minetest.chat_send_player(name, "Materials test complete")
+    end
+})
+
+--WARNING: only handles top level of table currently
+--TODO: handle nested tables
+minetest.register_chatcommand("xcompat_test_textures", {
+    description = "Test textures",
+    privs = {server=true},
+    func = function(name, _)
+        local reference_textures = textures["minetest"]
+
+        for gameid, game_textures in pairs(textures) do
+            for texture, _ in pairs(reference_textures) do
+                if not game_textures[texture] then
+                    minetest.chat_send_player(name, "Missing texture: "..texture.." in game: "..gameid)
+                end
+            end
+        end
+
+        minetest.chat_send_player(name, "Textures test complete")
     end
 })
