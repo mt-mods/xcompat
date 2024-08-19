655486f465704a49e4bc5349a599e7f8761cca69
Buckaroo Banzai <39065740+BuckarooBanzay@users.noreply.github.com>
2024-08-19 07:07:07 +0200

    register tests only if enabled (#38)
    Co-authored-by: BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>

--- mtt.lua
+++ mtt.lua
@@ -4,4 +4,7 @@ mtt.emerge_area({ x=0, y=0, z=0 }, { x=10, y=10, z=10 })
 
 -- check nodelist
 local mtt_nodelist = minetest.settings:get("mtt_nodelist")
-mtt.validate_nodenames(minetest.get_modpath("xcompat") .. "/test/nodelist/" .. mtt_nodelist)
+if mtt_nodelist then
+    -- nodelist specified, check if all the required nodes are present
+    mtt.validate_nodenames(minetest.get_modpath("xcompat") .. "/test/nodelist/" .. mtt_nodelist)
+end
