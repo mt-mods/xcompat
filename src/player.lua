13220db077d1fbed794841a8c715e53599d32e50
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2024-09-08 17:58:46 -0400

    fix copy paste comment issue

--- src/player.lua
+++ src/player.lua
@@ -1,6 +1,6 @@
 local filename = xcompat.gameid
 
---if we dont have a materials file for the game, use minetest
+--if we dont have a player file for the game, use minetest
 if not xcompat.utilities.file_exists(xcompat.modpath .. "/src/player/" .. filename .. ".lua") then
     filename = "xcompat_agnostic"
 end
