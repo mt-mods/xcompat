be3827470ac619e23bd75a767478fba8d6a54b2c
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2026-01-04 15:17:35 -0500

    simplify sounds/xcompat_agnostic.lua

--- src/sounds/xcompat_agnostic.lua
+++ src/sounds/xcompat_agnostic.lua
@@ -1,63 +1,13 @@
 local sound_api = {}
 
-function sound_api.node_sound_default(soundtable)
+local pass = function(soundtable)
     return soundtable
 end
 
-function sound_api.node_sound_stone_defaults(soundtable)
-    return soundtable
-end
-
-function sound_api.node_sound_dirt_defaults(soundtable)
-    return soundtable
-end
-
-function sound_api.node_sound_grass_defaults(soundtable)
-    return soundtable
-end
-
-function sound_api.node_sound_sand_defaults(soundtable)
-    return soundtable
-end
-
-function sound_api.node_sound_gravel_defaults(soundtable)
-    return soundtable
-end
-
-function sound_api.node_sound_wood_defaults(soundtable)
-    return soundtable
-end
-
-function sound_api.node_sound_leaves_defaults(soundtable)
-    return soundtable
-end
-
-function sound_api.node_sound_glass_defaults(soundtable)
-    return soundtable
-end
-
-function sound_api.node_sound_ice_defaults(soundtable)
-    return soundtable
-end
-
-function sound_api.node_sound_metal_defaults(soundtable)
-    return soundtable
-end
-
-function sound_api.node_sound_water_defaults(soundtable)
-    return soundtable
-end
-
-function sound_api.node_sound_lava_defaults(soundtable)
-    return soundtable
-end
-
-function sound_api.node_sound_snow_defaults(soundtable)
-    return soundtable
-end
-
-function sound_api.node_sound_wool_defaults(soundtable)
-    return soundtable
-end
+setmetatable(sound_api, {
+    __index = function()
+        return pass -- always return the same function
+    end
+})
 
 return sound_api
