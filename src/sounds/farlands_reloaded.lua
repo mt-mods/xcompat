41efef1b860555557e7adb701ef945f38ab16a0b
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2026-01-04 15:55:35 -0500

    fix comment

--- src/sounds/farlands_reloaded.lua
+++ src/sounds/farlands_reloaded.lua
@@ -5,8 +5,8 @@ end
 local sound_api = {
     node_sound_default         = pass,
     node_sound_stone_defaults  = fl_stone.sounds.stone,
-    node_sound_dirt_defaults   = fl_topsoil.sounds.grass,
-    node_sound_grass_defaults  = fl_topsoil.sounds.grass, --return grass as some games use dirt vs grass
+    node_sound_dirt_defaults   = fl_topsoil.sounds.grass, --return grass as some games use dirt vs grass
+    node_sound_grass_defaults  = fl_topsoil.sounds.grass,
     node_sound_sand_defaults   = fl_stone.sounds.sand,
     node_sound_gravel_defaults = fl_topsoil.sounds.gravel,
     node_sound_wood_defaults   = fl_trees.sounds.wood,
